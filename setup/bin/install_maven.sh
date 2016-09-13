9fbd2c7d14c222b88628d1ae0fbb95ed9dc324f4
James <jmedel@hortonworks.com>
2016-09-13 00:20:34 -0700

    update install_maven.sh
    - modified /root to /usr, so now maven gets downloaded to /usr path

--- setup/bin/install_maven.sh
+++ setup/bin/install_maven.sh
@@ -2,9 +2,9 @@
 
 wget http://apache.cs.utah.edu/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 mv apache-maven-3.3.9-bin.tar.gz /tmp
-tar xvzf /tmp/apache-maven-*-bin.tar.gz -C /root
-mv /root/apache-maven* /root/maven
+tar xvzf /tmp/apache-maven-*-bin.tar.gz -C /usr
+mv /usr/apache-maven* /usr/maven
 
-echo 'M2_HOME=/root/maven' >> ~/.bashrc
-echo 'M2=$M2_HOME/bin' >> ~/.bashrc
-echo 'PATH=$PATH:$M2' >> ~/.bashrc
+echo 'M2_HOME=/usr/maven' >> ~/.bash_profile
+echo 'M2=$M2_HOME/bin' >> ~/.bash_profile
+echo 'PATH=$M2:$PATH' >> ~/.bash_profile
